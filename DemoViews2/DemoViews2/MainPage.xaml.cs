af7e796d123aa3aa7265044ae3f9cd588bbb78d4
JuanCamiloRios <42045074+JuanCamiloRios@users.noreply.github.com>
2018-08-09 14:27:55 -0500

    Initial commit

---
+++ DemoViews2/DemoViews2/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoViews2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Browser_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+
+        }
+    }
+}
