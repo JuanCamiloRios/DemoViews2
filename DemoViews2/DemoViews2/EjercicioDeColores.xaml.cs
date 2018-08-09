699848694bb8c444b818ed6f27855b0e85920bd3
JuanCamiloRios <42045074+JuanCamiloRios@users.noreply.github.com>
2018-08-09 16:39:24 -0500

    MANEJO DE COLORES

--- DemoViews2/DemoViews2/EjercicioDeColores.xaml.cs
+++ DemoViews2/DemoViews2/EjercicioDeColores.xaml.cs
@@ -16,5 +16,23 @@ namespace DemoViews2
 		{
 			InitializeComponent ();
 		}
-	}
+
+        void CambiarColor(object sender,Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            var rojo = sldRojo.Value;
+            var azul = sldAzul.Value;
+            var verde = sldVerde.Value;
+            Color bgColor = new Color(rojo, verde, azul);
+            boxColor.BackgroundColor = bgColor;
+            ldDisplay.Text = ColorToHex(bgColor);
+        }
+        private string ColorToHex(Color color)
+        {
+            int rojo = (int)(color.R * 255);
+            int verde= (int)(color.G * 255);
+            int azul = (int)(color.B * 255);
+            int alpha = (int)(color.A * 255);
+            return $"#{rojo:X2}{verde:X2}{azul:X2}{alpha:X2}";
+        }
+    }
 }
